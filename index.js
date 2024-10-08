4ed275333778089e1a5cd89a0899cd64a4776092
Tristan Tritch <ttmeep101@hotmail.com>
2024-10-08 10:34:04 -0500

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if(blocks >= 42) {
+        return blocks - 42;
+    } else {
+        return (blocks - 42)*-1;
+    };
+}
+
+function distanceFromHqInFeet(val){
+    return distanceFromHqInBlocks(val)*264;
+}
+
+function distanceTravelledInFeet(start,end){
+    if(start-end >= 0){
+        return (start-end)*264;
+    } else {
+        return (start-end)*-264;
+    }
+}
+
+function calculatesFarePrice(start,end){
+    if(distanceTravelledInFeet(start,end) <= 400){
+        return 0;
+    } else if(distanceTravelledInFeet(start,end) <= 2000){
+        return 0.02*(distanceTravelledInFeet(start,end)-400);
+    } else if(distanceTravelledInFeet(start,end) <=2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    };
+}
